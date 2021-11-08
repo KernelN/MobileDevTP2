827a7a177fe2f3c9760735192fe050fc5f40b75a
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-08 13:51:41 -0300

    Add VirtualJoystick (WIP)
    It receives input, but doesn't send any output

---
+++ MobileDevTP2/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Action<Vector2> AxisInput;
+
+#region Unity Events
+        
+#endregion Unity Events
+}
---
+++ MobileDevTP2/Assets/Scripts/Input/VirtualJoystick.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler
+{
+    [SerializeField] RectTransform joystickHolder;
+    [SerializeField] RectTransform joystick;
+
+    #region Unity Events
+    private void Start()
+    {
+        joystick = GetComponent<RectTransform>();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        MoveStick(eventData.position);
+    }
+    #endregion Unity Events
+
+    void MoveStick(Vector2 pointerPosition)
+    {
+        Vector2 newPosition;
+        newPosition.x = GetStickPosX(pointerPosition.x - joystickHolder.position.x);
+        newPosition.y = GetStickPosY(pointerPosition.y - joystickHolder.position.y);
+        joystick.anchoredPosition = newPosition;
+    }
+    float GetStickPosX(float x)
+    {
+        if (Mathf.Abs(x) < joystickHolder.sizeDelta.x / 2)
+        {
+            return x;
+        }
+
+        if (x > joystickHolder.sizeDelta.x / 2)
+        {
+            return joystickHolder.sizeDelta.x / 2 - joystickHolder.sizeDelta.x / 10;
+        }
+        else
+        {
+            return -joystickHolder.sizeDelta.x / 2 + joystickHolder.sizeDelta.x / 10;
+        }
+    }
+    float GetStickPosY(float y)
+    {
+        if (Mathf.Abs(y) < joystickHolder.sizeDelta.y / 2)
+        {
+            return y;
+        }
+
+        if (y > joystickHolder.sizeDelta.y / 2)
+        {
+            return joystickHolder.sizeDelta.y / 2 - joystickHolder.sizeDelta.x / 10;
+        }
+        else
+        {
+            return -joystickHolder.sizeDelta.y / 2 + joystickHolder.sizeDelta.x / 10;
+        }
+    }
+}
