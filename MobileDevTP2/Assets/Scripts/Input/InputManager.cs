8770aa46ec5eeee211d0e9669cfc28af8649b97a
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-12 09:52:32 -0300

    Connect InputManager with Ship

--- MobileDevTP2/Assets/Scripts/Input/InputManager.cs
+++ MobileDevTP2/Assets/Scripts/Input/InputManager.cs
@@ -5,10 +5,14 @@ public class InputManager : MonoBehaviour
 {
     public Action<Vector2> AxisInputReceived;
     [SerializeField] Vector2 axisInput;
+    [SerializeField] ShipController player;
 
-#region Unity Events
-        
-#endregion Unity Events
+    #region Unity Events
+    private void Start()
+    {
+        AxisInputReceived += player.OnNewDirection;
+    }
+    #endregion Unity Events
 
     public void OnAxisInputReceived(Vector2 newInput)
     {
--- MobileDevTP2/Assets/Scripts/Ship/ShipController.cs
+++ MobileDevTP2/Assets/Scripts/Ship/ShipController.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    [SerializeField] float speed;
+    bool firstDirectionRecieved = false;
+
     // Update is called once per frame
     void Update()
     {
+        if (!firstDirectionRecieved) return;
         
+        transform.localPosition += transform.up * speed;
+    }
+
+    public void OnNewDirection(Vector2 direction)
+    {
+        firstDirectionRecieved = true;
+        transform.up = direction;
     }
 }
