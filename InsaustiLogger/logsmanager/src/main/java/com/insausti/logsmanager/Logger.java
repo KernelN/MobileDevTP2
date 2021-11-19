086773cc8750523450564df8be4a87e0eb1f071c
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-19 10:01:14 -0300

    Update Logger Plugin

---
+++ InsaustiLogger/logsmanager/src/main/java/com/insausti/logsmanager/Logger.java
@@ -0,0 +1,15 @@
+package com.insausti.logsmanager;
+
+import android.util.Log;
+
+public class Logger {
+    public static final Logger _instance = new Logger();
+
+    public static Logger GetInstance(){
+        return _instance;
+    }
+
+    public void SendLog(String msg) {
+        Log.d("L>", msg);
+    }
+}
