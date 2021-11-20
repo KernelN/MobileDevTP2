a3ce9a62c3e9f470d96d3058803695017491ea2c
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2021-11-20 03:28:49 -0300

    Add Touch/Gesture Manager

--- MobileDevTP2/Assets/Scripts/Input/InputManager.cs
+++ MobileDevTP2/Assets/Scripts/Input/InputManager.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     public Action<Vector2> AxisInputReceived;
-    [SerializeField] Vector2 axisInput;
+    public Action<Vector2> CircleInputReceived;
     [SerializeField] ShipController player;
+    [SerializeField] TurretController turret;
+    [SerializeField] Vector2 axisInput;
 
-    #region Unity Events
+    //Unity Events
     private void Start()
     {
         AxisInputReceived += player.OnNewDirection;
+        CircleInputReceived += turret.OnNewTargetArea;
     }
-    #endregion Unity Events
 
+    //Methods
+    Vector2 GetMidPosOfCircle(Vector2 firstTouch, Vector2 midTouch, Vector2 lastTouch)
+    {
+        //calculate midPos between first and mid touch
+        Vector2 firstToMid = Vector2.Lerp(firstTouch, midTouch, 0.5f);
+     
+        //calculate midPos between last and mid touch
+        Vector2 LastToMid = Vector2.Lerp(lastTouch, midTouch, 0.5f);
+
+        //calculate final midPos
+        return Vector2.Lerp(firstToMid, LastToMid, 0.5f); 
+    }
+
+    //Event Receivers
     public void OnAxisInputReceived(Vector2 newInput)
     {
         axisInput = newInput;
         AxisInputReceived?.Invoke(newInput);
+    }
+    public void OnGestureInputReceived(Vector2 firstPos, Vector2 midPos, Vector2 lastPos)
+    {
+        //if distance between first and last is less than first and mid, set the gesture as circle
+        if ((firstPos - lastPos).sqrMagnitude < (firstPos - midPos).sqrMagnitude)
+        {
+            //send mid of circle to turret
+            CircleInputReceived.Invoke(GetMidPosOfCircle(firstPos, midPos, lastPos));
+        }
     }
 }
---
+++ MobileDevTP2/Assets/Scripts/Input/TouchManager.cs
@@ -0,0 +1,70 @@
+﻿#if UNITY_ANDROID
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchManager : MonoBehaviour
+{
+    public Action<Vector2, Vector2, Vector2> NewGestureInput;
+    [SerializeField] InputManager manager;
+    [SerializeField] List<Vector2> touchPositions;
+    [SerializeField] LayerMask notTouchableMask;
+
+    //Methods Unity Events
+    private void Start()
+    {
+        NewGestureInput += manager.OnGestureInputReceived;
+    }
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            AddTouchToGestureList(Input.GetTouch(0));
+        }
+    }
+
+    //Methods
+    void AddTouchToGestureList(Touch touch)
+    {
+        // Handle finger movements based on touch phase.
+        switch (touch.phase)
+        {
+            // Record initial touch position if it's not touching the V-Stick neither the ship
+            case TouchPhase.Began:
+                 // Construct a ray from the current touch coordinates
+                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                //Add the touch position to the gesture's touch list if it didn't collide with any obstacles
+                if (!Physics.Raycast(ray, 100, notTouchableMask))
+                {                    
+                    touchPositions.Add(Camera.main.ScreenToWorldPoint(touch.position));
+                }
+                break;
+
+            //Add mid touch to gesture's touch positions list
+            case TouchPhase.Moved:
+                touchPositions.Add(Camera.main.ScreenToWorldPoint(touch.position));
+                break;
+
+            //Add last touch to gesture's touch positions list
+            case TouchPhase.Ended:
+                touchPositions.Add(Camera.main.ScreenToWorldPoint(touch.position));
+                BuildGesture();
+                break;
+        }
+    }
+    void BuildGesture()
+    {
+        //Get positions
+        Vector2 firstPos = touchPositions[0];
+        Vector2 midPos = touchPositions[touchPositions.Count / 2];
+        Vector2 lastPos = touchPositions[touchPositions.Count - 1];
+
+        //Clear list, as the values are no longer needed
+        touchPositions.Clear();
+
+        //Send output
+        NewGestureInput?.Invoke(firstPos, midPos, lastPos);
+    }
+}
+#endif
